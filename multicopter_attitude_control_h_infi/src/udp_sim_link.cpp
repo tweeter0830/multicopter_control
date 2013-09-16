76e3d5b73dcea3c8fe29007c0ae27811b9cffa69
U-CYMER\L503823 <L503823@csdpb7y8bx.intra.cymer.com>
2013-09-16 16:24:04 -0700

    Added a Makefile as well as a basic utility to test UDP commuincation.
    B
    B
    
    C
    
    c
    ;

---
+++ multicopter_attitude_control_h_infi/src/udp_sim_link.cpp
@@ -0,0 +1,53 @@
+//
+// client.cpp
+// ~~~~~~~~~~
+//
+// Copyright (c) 2003-2013 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <iostream>
+#include <string>
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::udp;
+
+int main(int argc, char* argv[])
+{
+	std::string str_internal_ip = "127.0.0.1";
+	try
+	{
+		// if (argc != 2)
+		// {
+		// 	std::cerr << "Usage: client <host>" << std::endl;
+		// 	return 1;
+		// }
+		std::cout<< "Started" << std::endl;
+		boost::asio::ip::address internal_ip = boost::asio::ip::address::from_string(str_internal_ip);
+		boost::asio::io_service io_service;
+		
+		udp::endpoint endpoint_recv_from(internal_ip,1230);
+		udp::endpoint endpoint_send_to(internal_ip,1230);
+
+		udp::socket socket(io_service);
+		socket.open(udp::v4());
+
+		boost::array<char, 1> send_buf  = {{ 0 }};
+		socket.send_to(boost::asio::buffer(send_buf), endpoint_send_to);
+		std::cout<< "Data '" << send_buf << "' sent" << std::endl;
+		boost::array<char, 128> recv_buf;
+		size_t len = socket.receive_from(
+			boost::asio::buffer(recv_buf), endpoint_send_to);
+
+		std::cout.write(recv_buf.data(), len);
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	return 0;
+}
